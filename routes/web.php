a787bb5b9a85f4dda27c5aa73b438a5c144352b7
Furkan Eraslan <eraslanfurkan23@gmail.com>
2026-03-17 04:18:29 +0200

    Route logic changed.

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\GameController;
 
 Route::get('/', function () { return view('welcome'); }); // Your landing page
-Route::get('/game/start', [GameController::class, 'start'])->name('game.start');
+Route::match(['get', 'post'], '/game/start', [GameController::class, 'start'])->name('game.start');
 Route::get('/game/{id}', [GameController::class, 'show'])->name('game.show');
 Route::post('/game/{id}/play/{index}', [GameController::class, 'playCard'])->name('game.play');
 Route::post('/game/{id}/flee', [GameController::class, 'flee'])->name('game.flee');
